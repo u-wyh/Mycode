5f18896da132f9fc236b1c120dc875f8ca7b6558
wuyuhui <wyh15026582592@outlook.com>
2025-07-16 12:34:05 +0800

    Refactor and reorganize KMP algorithm implementations
    Deleted legacy C and specialized KMP files, added a new 1-based KMP implementation, and renamed existing KMP C++ files for clarity. Updated comments and logic in the remaining files to clarify next array usage and border cases, improving maintainability and readability.

--- KMP/KMP算法——模板.cpp
+++ KMP/KMP算法——模板.cpp
@@ -1,4 +1,6 @@
-//P3375
+//https://www.luogu.com.cn/problem/P3375
+//注意这道题中的next数组要算到m   也就是s[i]的border存储在next[i+1]
+//这个程序中的next数组含义是 nxt[i]表示从0~i-1位置上的最大前后缀相同长度
 #include<bits/stdc++.h>
 using namespace std;
 const int MAXN = 1e6+5;
@@ -9,6 +11,10 @@ int Next[MAXN];
 int n,m;
 
 // 计算next数组
+//计算next数组时   存在一种nextval优化 
+//即如果跳到下一个位置和这一个位置是一样的  那么这个位置不行  下一个位置也是不行的
+//所以可以继续跳   但是这道题不行  因为这道题是多次匹配  即s2一个位置上的字符可能属于多个匹配
+//比如  ababa   abababa
 void nextArray() {
     if (m == 1) {
         Next[0] = -1;
@@ -52,8 +58,10 @@ void kmp() {
             cout<<x-y+1<<endl;
             y=Next[y];
         }
-        if((n-x)<(m-y))
+        if((n-x)<(m-y)){
+            //根据长度进行剪枝
             break;
+        }
     }
 }
 
