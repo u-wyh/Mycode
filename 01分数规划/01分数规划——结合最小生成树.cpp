27868f6e56ec4d1ab6a7dcea72c82c0147503591
wuyuhui <wyh15026582592@outlook.com>
2025-11-03 18:03:36 +0800

    Update comments in multiple C++ solution files
    Replaced non-ASCII/garbled comments with clear Chinese explanations in three C++ files, improving code readability and maintainability. No logic or functional changes were made. Also removed trailing newlines at end of files for consistency.

--- 01分数规划/01分数规划——结合最小生成树.cpp
+++ 01分数规划/01分数规划——结合最小生成树.cpp
@@ -1,15 +1,15 @@
-// ���ű���������
-// һ����n����ׯ��ÿ����ׯ��(x, y, z)��ʾ
-// ����(x,y)������ׯ�ڶ�ά��ͼ�е�λ�ã�z�����亣�θ߶�
-// ����������ׯ֮��ľ�����Ƕ�ά��ͼ�е�ŷʽ����
-// ����������ׯ֮�����·���Ѿ��Ǻ��β�ֵ�ľ���ֵ
-// ����������д�ׯ��ͨ������ϣ����·�����������٣�ͬʱϣ����
-// �ܻ��� / �ܾ��룬�����ֵ����С��������С�ı�ֵ�Ƕ��٣��������С�����3λ���ಿ������
+// 最优比率生成树
+// 一共有n个村庄，每个村庄由(x, y, z)表示
+// 其中(x,y)代表村庄在二维地图中的位置，z代表其海拔高度
+// 任意两个村庄之间的距离就是二维地图中的欧式距离
+// 任意两个村庄之间的修路花费就是海拔差值的绝对值
+// 现在想把所有村庄连通起来，希望修路的条数尽量少，同时希望让
+// 总花费 / 总距离，这个比值尽量小，返回最小的比值是多少，结果保留小数点后3位其余部分舍弃
 // 2 <= n <= 10^3
-// 0 <= x��y <= 10^4
+// 0 <= x、y <= 10^4
 // 0 <= z <= 10^7
-// �������� : http://poj.org/problem?id=2728
-// �ύ���µ�code���ύʱ��������ĳ�"Main"������ͨ�����в�������
+// 测试链接 : http://poj.org/problem?id=2728
+// 提交以下的code，提交时请把类名改成"Main"，可以通过所有测试用例
 #include<bits/stdc++.h>
 using namespace std;
 const int MAXN = 1001;
@@ -24,20 +24,20 @@ bool visit[MAXN];
 double value[MAXN];
 int n;
 
-// �ڽӾ���ṹ�µ�prim�㷨���ӽڵ�1�����õ���С��������Ȩֵ��
+// 邻接矩阵结构下的prim算法，从节点1出发得到最小生成树的权值和
 double prim(double x) {
-    // ����ֵ����Ȩ������ͳ�ƾ���
-    // ����ֻ�ͱ�Ȩ�йأ�ԭʼ��dist��cost����Ҫ
-    // ��1�ŵ���������µ����е�ľ��룬Ҳ����value����
+    // 结余值做边权，进而统计距离
+    // 距离只和边权有关，原始的dist、cost不重要
+    // 从1号点出发，更新到所有点的距离，也就是value数组
     for (int i = 1; i <= n; i++) {
         visit[i] = false;
         value[i] = cost[1][i] - x * dist[1][i];
     }
     visit[1] = true;
     double sum = 0;
-    // ��С������һ����n-1���ߣ�����һ����n-1�ֽ�����ÿ�ν����µĵ������С�������ĵ㼯
+    // 最小生成树一定有n-1条边，所以一共有n-1轮解锁，每次解锁新的点进入最小生成树的点集
     for (int i = 1; i <= n - 1; i++) {
-        // ��û�н����ĵ��У��ҵ�����С�������ĵ㼯����ĵ㣬���н���
+        // 在没有解锁的点中，找到离最小生成树的点集最近的点，进行解锁
         double minDist = INT_MAX;
         int next = 0;
         for (int j = 1; j <= n; j++) {
@@ -46,17 +46,17 @@ double prim(double x) {
                 next = j;
             }
         }
-        // ��С�ı߽�����С�������ı߼��������ĵ������С�������ĵ㼯
+        // 最小的边进入最小生成树的边集，解锁的点进入最小生成树的点集
         sum += minDist;
         visit[next] = true;
-        // �鿴�µĽ������ܲ�������������ľ���
+        // 查看新的解锁点能不能拉进其他点的距离
         for (int j = 1; j <= n; j++) {
             if (!visit[j] && value[j] > cost[next][j] - x * dist[next][j]) {
                 value[j] = cost[next][j] - x * dist[next][j];
             }
         }
     }
-    // ������С��������Ȩֵ��
+    // 返回最小生成树的权值和
     return sum;
 }
 
@@ -78,8 +78,8 @@ int main()
         double l = 0, r = 100, x, ans = 0;
         while (l < r && r - l >= sml) {
             x = (l + r) / 2;
-            // �����С��������Ȩֵ <= 0����꣬��������Ѱ��С��ֵ
-            // �����С��������Ȩֵ > 0������꣬�Ҳ������Ѱ�����ֵ
+            // 如果最小生成树的权值 <= 0，达标，左侧二分找寻更小比值
+            // 如果最小生成树的权值 > 0，不达标，右侧二分找寻更大比值
             if (prim(x) <= 0) {
                 ans = x;
                 r = x - sml;
